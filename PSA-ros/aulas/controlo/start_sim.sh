0757d3bf347b628a5aae24f3a3a4fd2a0c9da82a
Joao <joao@hp>
2024-10-29 22:55:33 +0000

    script

--- PSA-ros/aulas/controlo/start_sim.sh
+++ PSA-ros/aulas/controlo/start_sim.sh
@@ -7,5 +7,6 @@ echo "Waiting..."
 rosparam set robot_description -t pend.urdf
 rosparam load control.yaml
 rosrun gazebo_ros spawn_model -file pend.urdf -urdf -z 0 -model my_pend
-#rosrun controller_manager controller_manager spawn /pend_robot/joint_state_controller
-#rosrun controller_manager controller_manager spawn /pend_robot/effort_controller
+rosrun controller_manager controller_manager &
+rosservice call /pend_robot/controller_manager/load_controller "name: 'joint_state_controller'"
+rosservice call /pend_robot/controller_manager/load_controller "name: 'effort_controller'"
