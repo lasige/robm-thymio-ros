beb7cd37fd7b53a848772aa2047ddbc963cc0025
Joao <joao@hp>
2024-10-29 22:52:59 +0000

    script

--- PSA-ros/aulas/controlo/start_sim.sh
+++ PSA-ros/aulas/controlo/start_sim.sh
@@ -1,7 +1,11 @@
 #!/bin/bash 
 source /ros/devel/setup.bash
 roslaunch gazebo_ros empty_world.launch &
+echo "Waiting..."
+sleep 15
+echo "Waiting..."
 rosparam set robot_description -t pend.urdf
+rosparam load control.yaml
 rosrun gazebo_ros spawn_model -file pend.urdf -urdf -z 0 -model my_pend
-rosrun controller_manager controller_manager spawn /pend_robot/joint_state_controller
-rosrun controller_manager controller_manager spawn /pend_robot/effort_controller
+#rosrun controller_manager controller_manager spawn /pend_robot/joint_state_controller
+#rosrun controller_manager controller_manager spawn /pend_robot/effort_controller
