fcbbae0388008045376c6d31eb610fc3eee48f11
Joao <joao@hp>
2024-10-30 00:21:53 +0000

    plot

--- PSA-ros/aulas/controlo/start_sim.sh
+++ PSA-ros/aulas/controlo/start_sim.sh
@@ -11,8 +11,5 @@ rosrun controller_manager controller_manager &
 rosservice call /pend_robot/controller_manager/load_controller "name: 'joint_state_controller'"
 rosservice call /pend_robot/controller_manager/load_controller "name: 'effort_controller'"
 sleep 2
-rosservice call /pend_robot/controller_manager/switch_controller "start_controllers: ['joint_state_controller','effort_controller']
-stop_controllers: ['']
-strictness: 1
-start_asap: true
-timeout: 0.0"
+rosservice call /pend_robot/controller_manager/switch_controller {"start_controllers: ['joint_state_controller','effort_controller'],stop_controllers: [''],strictness: 1,start_asap: true,timeout: 0.0"}
+rqt_plot /pend_robot/joint_states/position[0],/pend_robot/joint_states/effort[0] &
