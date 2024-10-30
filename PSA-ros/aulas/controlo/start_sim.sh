e3cda64a055263c230dfd9436dcc68933874231f
Joao <joao@hp>
2024-10-30 00:05:36 +0000

    changes sim

--- PSA-ros/aulas/controlo/start_sim.sh
+++ PSA-ros/aulas/controlo/start_sim.sh
@@ -10,3 +10,9 @@ rosrun gazebo_ros spawn_model -file pend.urdf -urdf -z 0 -model my_pend
 rosrun controller_manager controller_manager &
 rosservice call /pend_robot/controller_manager/load_controller "name: 'joint_state_controller'"
 rosservice call /pend_robot/controller_manager/load_controller "name: 'effort_controller'"
+sleep 2
+rosservice call /pend_robot/controller_manager/switch_controller "start_controllers: ['joint_state_controller','effort_controller']
+stop_controllers: ['']
+strictness: 1
+start_asap: true
+timeout: 0.0"
